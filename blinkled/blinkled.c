aff38488d4d858850736dcc6df26e78ccb746da0
tassavarat <tassavarat@pm.me>
2021-01-30 16:07:18 -0800

    Readability changes

--- blinkled/blinkled.c
+++ blinkled/blinkled.c
@@ -1,18 +1,18 @@
 #include "rpi.h"
+#include <sys/stat.h>
 
-#define p1	17
+#define LED_PIN	17
 
 int main()
 {
 	volatile unsigned int *gpio;
 
 	gpio_setup(&gpio);
-	INP_GPIO(p1), OUT_GPIO(p1);
-
+	INP_GPIO(LED_PIN), OUT_GPIO(LED_PIN);
 	while (1) {
-		GPIO_SET = 1 << p1;
+		GPIO_SET = 1 << LED_PIN;
 		sleep(1);
-		GPIO_CLR = 1 << p1;
+		GPIO_CLR = 1 << LED_PIN;
 		sleep(1);
 	}
 	return 0;
--- blinkled/rpi.h
+++ blinkled/rpi.h
@@ -10,8 +10,8 @@
 #define BLOCK_SIZE		4 * 1024
 
 /* GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) */
-#define INP_GPIO(g)	*(gpio + g / 10) &= ~(7 << g % 10 * 3)
-#define OUT_GPIO(g)	*(gpio + g / 10) |=   1 << g % 10 * 3
+#define INP_GPIO(g)	*(gpio + (g / 10)) &= ~(7 << (g % 10 * 3))
+#define OUT_GPIO(g)	*(gpio + (g / 10)) |=  (1 << (g % 10 * 3))
 
 #define GPIO_SET	*(gpio + 7)	/* sets bits which are 1, ignores bits which are 0 */
 #define GPIO_CLR	*(gpio + 10)	/* clears bits which are 1, ignores bits which are 0 */
