32ffc7356baa63e8521ba4710b90f2722b699838
tassavarat <tassavarat@pm.me>
2021-03-13 17:43:51 -0800

    Validating pin

--- include/rpi.h
+++ include/rpi.h
@@ -12,6 +12,8 @@
 #define GPIO_BASE		BCM2711_PERI_BASE + 0x200000
 #define GPIO_PWM		BCM2711_PERI_BASE + 0x20C000
 
+#define VALID_PIN(g)		(g < 28)
+
 /* GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) */
 #define INP_GPIO(g)		*(gpio + (g / 10)) &= ~(7 << (g % 10 * 3))
 #define OUT_GPIO(g)		*(gpio + (g / 10)) |=  (1 << (g % 10 * 3))
@@ -19,6 +21,8 @@
 #define GPIO_SET		*(gpio + 7)	/* sets bits which are 1, ignores bits which are 0 */
 #define GPIO_CLR		*(gpio + 10)	/* clears bits which are 1, ignores bits which are 0 */
 #define GPIO_READ(g)		*(gpio + 13) & (1 << g)
+
+#define MAX_PINS		64
 
 enum {
 	GPIO_INP,
@@ -32,5 +36,6 @@ int gpio_dir(const unsigned int pin, const int dir);
 int gpio_set(const unsigned int pin, const int mode);
 int gpio_pud(const unsigned int pin, const int mode);
 unsigned int gpio_read(const unsigned int pin);
+int soft_pwm(const unsigned int pin, const unsigned int val, const unsigned int range);
 
 #endif /* RPI_H */
--- lib/rpi.c
+++ lib/rpi.c
@@ -1,6 +1,31 @@
 #include "../include/rpi.h"
 
 static volatile unsigned int *gpio;
+static volatile unsigned int pin_arr[MAX_PINS];
+
+int soft_pwm(const unsigned int pin, const unsigned int val, const unsigned int range)
+{
+	int res, *pin_ptr;
+
+	if (!VALID_PIN(pin) || pin >= MAX_PINS)
+		return 1;
+	
+	if (range <= 0)
+		return 1;
+
+	/* may change to 1 */
+	if (pin_arr[pin] != 0)
+		return 1;
+
+	pin_ptr = malloc(sizeof(*pin_ptr));
+	if (pin_ptr == NULL)
+		return 1;
+
+	gpio_set(pin, 0);
+	gpio_dir(pin, GPIO_OUT);
+
+	return 0;
+}
 
 /**
  * gpio_read - read value of pin
@@ -10,6 +35,9 @@ static volatile unsigned int *gpio;
  */
 unsigned int gpio_read(const unsigned int pin)
 {
+	if (!VALID_PIN(pin))
+		return 1;
+
 	return GPIO_READ(pin);
 }
 
@@ -17,11 +45,13 @@ unsigned int gpio_read(const unsigned int pin)
  * gpio_pud - pull pin up or down
  * @pin:	pin to pull
  * @mode:	specifies up or down
+ *
+ * Return:	0 on success, 1 on failure
  */
-void gpio_pud(const unsigned int pin, const int mode)
+int gpio_pud(const unsigned int pin, const int mode)
 {
-	if (mode != PULL_UP && mode != PULL_DOWN)
-		return;
+	if ((mode != PULL_UP && mode != PULL_DOWN) || !VALID_PIN(pin))
+		return 1;
 
 	int pull_reg, pull_shift;
 	unsigned int pull_bits;
@@ -32,60 +62,76 @@ void gpio_pud(const unsigned int pin, const int mode)
 	pull_bits &= ~(3 << pull_shift);
 	pull_bits |= (mode << pull_shift);
 	*(gpio + pull_reg) = pull_bits;
+	return 0;
 }
 
 /**
  * gpio_set - set or clear gpio bits
  * @pin:	pin to modify
  * @mode:	specifies set or clear
+ *
+ * Return:	0 on success, 1 on failure
  */
-void gpio_set(const unsigned int pin, const int mode)
+int gpio_set(const unsigned int pin, const int mode)
 {
+	if (!VALID_PIN(pin))
+		return 1;
+
 	if (mode == 1)
 		GPIO_SET = 1 << pin;
 	else if (mode == 0)
 		GPIO_CLR = 1 << pin;
 	else
-		return;
+		return 1;
+	return 0;
 }
 
 /**
  * gpio_dir - set gpio pin to input or output
  * @pin:	pin to set
  * @dir:	specifies input or output
+ *
+ * Return:	0 on success, 1 on failure
  */
-void gpio_dir(const unsigned int pin, const int dir)
+int gpio_dir(const unsigned int pin, const int dir)
 {
+	if (!VALID_PIN(pin))
+		return 1;
+
 	if (dir == GPIO_INP) {
 		INP_GPIO(pin);
 	} else if (dir == GPIO_OUT) {
 		INP_GPIO(pin);
 		OUT_GPIO(pin);
 	} else {
-		return;
+		return 1;
 	}
+	return 0;
 }
 
 /**
  * gpio_setup - set up memory regions to access GPIO
  * @gpio:	double pointer to mapped area
+ *
+ * Return:	0 on success, 1 on failure
  */
-void gpio_setup()
+int gpio_setup()
 {
 	int fd;
 	void *map;
 
 	if ((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) {
 		perror("open() error");
-		exit(1);
+		return 1;
 	}
 
 	map = mmap(NULL, sysconf(_SC_PAGE_SIZE), PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO_BASE);
 	close(fd);
 	if (map == MAP_FAILED) {
 		perror("mmap() error");
-		exit(1);
+		return 1;
 	}
 
 	gpio = map;
+	return 0;
 }
